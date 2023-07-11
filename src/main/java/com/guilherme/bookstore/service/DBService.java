77616b5b02bb6ac1a3a852fc19c89349d1f29792
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-11 20:08:33 -0300

    perfil de teste implementado

---
+++ src/main/java/com/guilherme/bookstore/service/DBService.java
@@ -0,0 +1,40 @@
+package com.guilherme.bookstore.service;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.bookstore.domain.Categoria;
+import com.guilherme.bookstore.domain.Livro;
+import com.guilherme.bookstore.repositories.CategoriaRepository;
+import com.guilherme.bookstore.repositories.LivroRepository;
+
+@Service
+public class DBService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private LivroRepository livroRepository;
+	
+	public void instanciaBaseDeDados() {
+		
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Categoria cat2 = new Categoria(null, "Ciências", "Livros de ciências");
+		Categoria cat3 = new Categoria(null, "História", "Livros de história");
+		
+		Livro l1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem ipsum", cat1);
+		Livro l2 = new Livro(null, "Ciências biologicas", "Ramon Dino", "Lorem ipsum", cat2);
+		Livro l3 = new Livro(null, "A história do Brasil", "Theo Afonso", "Lorem ipsum", cat3);
+		
+		cat1.getLivros().addAll(Arrays.asList(l1));
+		cat2.getLivros().addAll(Arrays.asList(l2));
+		cat3.getLivros().addAll(Arrays.asList(l3));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		this.livroRepository.saveAll(Arrays.asList(l1, l2, l3));
+		
+	}
+
+}
