df3159f4fe599cf582a55292adb2a7521219fe28
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-12 10:05:23 -0300

    findById para Categoria implementado

--- src/main/java/com/guilherme/bookstore/domain/Livro.java
+++ src/main/java/com/guilherme/bookstore/domain/Livro.java
@@ -3,6 +3,8 @@ package com.guilherme.bookstore.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +25,7 @@ public class Livro implements Serializable{
 	private String nome_autor;
 	private String texto;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
