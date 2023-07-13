0d14b14a19a7a314de4daf3e7298e323e92bfb58
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-13 08:33:13 -0300

    metodos de get, post, update e delete implementados para os livros

---
+++ src/main/java/com/guilherme/bookstore/service/LivroService.java
@@ -0,0 +1,47 @@
+package com.guilherme.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.bookstore.domain.Livro;
+import com.guilherme.bookstore.dtos.LivroDTO;
+import com.guilherme.bookstore.repositories.LivroRepository;
+import com.guilherme.bookstore.service.exceptions.ObjectNotFoundException;
+
+import java.util.List;
+
+@Service
+public class LivroService {
+
+	@Autowired
+	private LivroRepository repository;
+
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Livro não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
+	}
+	
+	public List<Livro> findAll() {
+		return repository.findAll();
+	}
+	
+	public Livro update(Integer id, LivroDTO objDto) {
+		Livro obj = findById(id);
+		obj.setTitulo(objDto.getTitulo());
+		obj.setNome_autor(objDto.getNome_autor());
+		obj.setTexto(objDto.getTexto());
+		return repository.save(obj);
+	}
+	
+	public Livro create(Livro obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+}
