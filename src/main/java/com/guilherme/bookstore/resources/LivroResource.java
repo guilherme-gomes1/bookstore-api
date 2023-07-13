d055892bd2468023c767cae90a37b82d5452b4e9
Guilherme Gomes <guilhermegg018@gmail.com>
2023-07-13 10:59:54 -0300

    atualizando o metodo update para conseguir alterar tambem o autor e o texto do livro, e adicionando o metodo updatePatch

--- src/main/java/com/guilherme/bookstore/resources/LivroResource.java
+++ src/main/java/com/guilherme/bookstore/resources/LivroResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -56,9 +57,15 @@ public class LivroResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<LivroDTO> update(@PathVariable Integer id, @RequestBody LivroDTO objDto) {
-		Livro newObj = service.update(id, objDto);
-		return ResponseEntity.ok().body(new LivroDTO(newObj));
+	public ResponseEntity<Livro> update(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<Livro> updatePatch(@PathVariable Integer id, @RequestBody Livro obj) {
+		Livro newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
 	}
 	
 	@DeleteMapping(value = "/{id}")
--- src/main/java/com/guilherme/bookstore/service/LivroService.java
+++ src/main/java/com/guilherme/bookstore/service/LivroService.java
@@ -1,23 +1,21 @@
 package com.guilherme.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.bookstore.domain.Livro;
-import com.guilherme.bookstore.dtos.LivroDTO;
 import com.guilherme.bookstore.repositories.LivroRepository;
 import com.guilherme.bookstore.service.exceptions.ObjectNotFoundException;
-
-import java.util.List;
 
 @Service
 public class LivroService {
 
 	@Autowired
 	private LivroRepository repository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
 
@@ -26,27 +24,34 @@ public class LivroService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Livro não encontrado! Id: " + id + ", Tipo: " + Livro.class.getName()));
 	}
-	
+
 	public List<Livro> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public List<Livro> findAllByCategory(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategory(id_cat);
 	}
-	
-	public Livro update(Integer id, LivroDTO objDto) {
-		Livro obj = findById(id);
-		obj.setTitulo(objDto.getTitulo());
-		return repository.save(obj);
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
 	}
-	
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNome_autor(obj.getNome_autor());
+		newObj.setTexto(obj.getTexto());
+
+	}
+
 	public Livro create(Livro obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		repository.deleteById(id);
 	}
